aefc3e6c8ae8b41f059d5d9485bb57d87aa6115c
Abdulwarith <adebayoayomide646@gmail.com>
2023-12-01 08:51:06 -0800

    initial commit

--- backend/app.js
+++ backend/app.js
@@ -1,1 +1,15 @@
-console.log('hello')
+require('dotenv').config()
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+app.use(express.static(path.resolve(__dirname, "../public")));
+
+
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../public', 'index.html'))
+})
+
+const port = process.env.PORT
+app.listen(port, () => console.log("Server started..."));
